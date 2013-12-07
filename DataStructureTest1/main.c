98045b49c01e733071856330f8ec4775c8da6154
Sun PeiFeng <g7733pf@gmail.com>
2013-12-07 15:44:14 +0800

    两套链表,双向的还在当单向的用…

--- DataStructureTest1/main.c
+++ DataStructureTest1/main.c
@@ -20,8 +20,8 @@ typedef struct LNode{
 
 typedef struct TWNode{
     int data;
-    struct TWNode *prev;
     struct TWNode *next;
+    struct TWNode *prev;
 }TWNode, *TwoWayLinkedList;//双向链表
 
 
@@ -71,35 +71,45 @@ OneWaylinkList initOneWayListWithLength(int lengthOfTheCreatingList)
     free(list);
 }
 
-void initTwoWayListWithLength(int lengthOfTheCreatingList)
+TwoWayLinkedList initTwoWayListWithLength(int lengthOfTheCreatingList)
 {
-    TwoWayLinkedList p = NULL,r = NULL,q = NULL,list = NULL;
-    int i = 1;
+    TwoWayLinkedList p = NULL, r = NULL, list = NULL;//p和r和list各是一个结构体
+    //p负责建立新节点,r负责
     
-    for (i = 1; i <= lengthOfTheCreatingList; i++)
+    list = (TwoWayLinkedList)malloc(sizeof(TwoWayLinkedList));
+    
+    list->next = NULL;//建立了一个只有一个元素的list
+    
+    for (int i = 1; i <= lengthOfTheCreatingList; i++)
     {
-        p = (TwoWayLinkedList)malloc(sizeof(TwoWayLinkedList));
-        p->data = i;
-        p->next = NULL;
+        
+        p = (TwoWayLinkedList)malloc(sizeof(TwoWayLinkedList));//每次循环都为p新分配一个空间,不再影响已被引用过的链表元素位置
+        
+        p->data = i;//指定p内的内容
         p->prev = NULL;
+        p->next = NULL;//p指向下一个的指针为空
+        
         if (i == 1)
         {
-            list = p;
-        }
-        else
-        {
-            p->prev = p;
-            r = p;
-            r->data =
-            r->next =
             
+            list->next = p;//这里是指针(地址)传递!两处指向的地址是一致的!
+            
+            //在第一次循环中,list的next被指向了刚创建的数据p的位置,在此语句完成赋值
         }
-        r = (TwoWayLinkedList)malloc(sizeof(TwoWayLinkedList));
-        p->next = r;//r是下一个节点
         
+        else if(i >= 2)
+        {
+            
+            r->next = p;//指针传递!两处指向的地址是一致的!
+            
+            //第二次循环中,r的next被指向了刚创建的数据p的位置,同时指定了list->next->next的地址,即在此处完成赋值
+            //每次循环中p都是新的,即是在第二次循环中,本语句指出了list->next->next的位置
+        }
+        
+        r = p;	//p的地址传给r,r中含有本次循环赋来的数据,第一次循环时即为list->next的地址传入了r中
+        //上条else语句中p的地址给了r->next,本句中把p的地址给了r而不是它的子节点,相当于把r向下一个节点推进了(待修正)
     }
-    
-    
+    return list;
 }
 
 void printLinkedList(OneWaylinkList listToBePrinted)
@@ -121,12 +131,31 @@ void printLinkedList(OneWaylinkList listToBePrinted)
     free(p);
 }
 
+void printTwoWayLinkedList(TwoWayLinkedList listToBePrinted)
+{
+    TwoWayLinkedList p = NULL;
+    
+    while (listToBePrinted->next)
+    {
+        
+        printf("%i\n",listToBePrinted->data);
+        p = (TwoWayLinkedList)malloc(sizeof(TwoWayLinkedList));
+        p = listToBePrinted->next;
+        listToBePrinted = p;
+        
+    }
+    
+    printf("%i\n",listToBePrinted->data);
+    
+    free(p);
+}
 
 int main(int argc, char const *argv[])
 {
     
     
-    printLinkedList(initOneWayListWithLength(90000));
+    //printLinkedList(initOneWayListWithLength(900));
+    printTwoWayLinkedList(initTwoWayListWithLength(900));
     return 0;
     
     
